2bd20c48a7b9930a64042771f513993d6d7fd5c0
spursy <582214816@qq.com>
2017-08-02 23:09:00 +0800

    add babel demo.

---
+++ webpack4.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var htmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // //插件项
+    // plugins: [commonsPlugin],
+    //页面入口文件配置
+    entry: './src1/app.js',
+    //入口文件输出配置
+    output: {
+        path: '/Users/spursy/Develop/util-webpack/dist',
+        filename: 'js/[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader:'babel-loader',
+                exclude:/node_modules/,
+                include:/src/,
+                options: {
+                    presets: ['env']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebPackPlugin({
+            filename: 'index.html',
+            'template': 'index4.html',
+            inject: 'body'
+        })
+    ]
+};
